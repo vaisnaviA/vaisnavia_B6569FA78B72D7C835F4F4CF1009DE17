0642297cb6ee7299923bd1d9f35e249305ab73e9
vaisnaviA <144367197+vaisnaviA@users.noreply.github.com>
2023-09-15 12:03:06 +0530

    Add files via upload

--- main (6).py
+++ main (6).py
@@ -1,14 +1,33 @@
-def recur_factorial(n):
-  if n == 1:
-    return n
-  else:
-    return n * recur_factorial(n - 1)
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount} successfully. New balance: {self.balance}")
+    
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew {amount} successfully. New balance: {self.balance}")
+        else:
+            print("Insufficient balance.")
+    
+    def get_balance(self):
+        return self.balance
 
-    num = 7
 
-    if num < 0:
-      print("Sorry, factorial does not exist for negative numbers")
-    elif num == 0:
-      print("The factorial of 0 is 1")
-    else:
-      print("The factorial of", num, "is", recur_factorial(num))
+# Create an instance of the BankAccount class
+account = BankAccount("1234567890", 1000)
+
+# Test deposit functionality
+account.deposit(500)
+
+# Test withdrawal functionality
+account.withdraw(200)
+account.withdraw(1500)
+
+# Get current balance
+balance = account.get_balance()
+print(f"Current balance: {balance}")
---
+++ main (7).py
@@ -0,0 +1,15 @@
+class Batsman:
+    def play(self):
+        print("Batsman is playing.")
+
+class Bowler:
+    def play(self):
+        print("Bowler is playing.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
